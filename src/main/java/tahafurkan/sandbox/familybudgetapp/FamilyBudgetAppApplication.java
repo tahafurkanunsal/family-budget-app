e0a45a0d916469a09525bca6bf22c3da23c60fb5
Taha Furkan Ünsal <t.furkanunsal@gmail.com>
2022-12-25 14:52:36 +0300

    MySQL database connection established.

---
+++ src/main/java/tahafurkan/sandbox/familybudgetapp/FamilyBudgetAppApplication.java
@@ -0,0 +1,13 @@
+package tahafurkan.sandbox.familybudgetapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyBudgetAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyBudgetAppApplication.class, args);
+	}
+
+}
