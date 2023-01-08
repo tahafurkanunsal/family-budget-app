63f59a5ac513c8e65ff67dd980ef627fa69125cb
Taha Furkan Ünsal <t.furkanunsal@gmail.com>
2023-01-08 15:28:14 +0300

    created unit test for database.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO spendings (id,  date , price ,type ,billing_type ,user_id) VALUES (106, '2001-06-15' , 100 ,  1, NULL , 1 );
+INSERT INTO spendings (id,  date , price ,type ,billing_type ,user_id) VALUES (107, '2001-06-15' , 200 ,  0, NULL , 1 );
+INSERT INTO spendings (id,  date , price ,type ,billing_type ,user_id) VALUES (108, '2001-06-15' , 300 ,  0, NULL , 1 );
+INSERT INTO spendings (id,  date , price ,type ,billing_type ,user_id) VALUES (109, '2001-06-15' , 900 ,  0, NULL , 2 );
+INSERT INTO spendings (id,  date , price ,type ,billing_type ,user_id) VALUES (110, '2001-06-15' , 400 ,  0, NULL , 1 );
+INSERT INTO spendings (id,  date , price ,type ,billing_type ,user_id) VALUES (111, '2001-06-15' , 1200,  0, NULL , 3 );
---
+++ src/test/java/tahafurkan/sandbox/familybudgetapp/repository/SpendingRepositoryTest.java
@@ -0,0 +1,33 @@
+package tahafurkan.sandbox.familybudgetapp.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+import tahafurkan.sandbox.familybudgetapp.model.Spending;
+
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class SpendingRepositoryTest {
+
+    @Autowired
+    SpendingRepository spendingRepository;
+
+    @Test
+    @Sql({"/data.sql"})
+    void findMostSpendingDetailsByDate_GivenDate_ReturnSpendingDetails() {
+        Date startDate = new Date(2001,06,1);
+        Date endDate = new Date(2001,06,30);
+        List<Spending> spendings = spendingRepository.findMostSpendingDetailsByDate(startDate , endDate);
+        assertNotNull(spendings);
+        assertEquals(1 , spendings.size());
+
+    }
+
+    }
