0a149ab6098b733a6b455004ce95f119aadee358
Taha Furkan Ünsal <t.furkanunsal@gmail.com>
2023-01-09 19:52:05 +0300

    deleted test method re-added.

--- src/test/java/tahafurkan/sandbox/familybudgetapp/repository/SpendingRepositoryTest.java
+++ src/test/java/tahafurkan/sandbox/familybudgetapp/repository/SpendingRepositoryTest.java
@@ -5,7 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.jdbc.Sql;
+import tahafurkan.sandbox.familybudgetapp.model.dto.UserSpendingDetails;
 
+import java.util.Date;
+import java.util.List;
+
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -16,6 +22,11 @@ class SpendingRepositoryTest {
 
     @Test
     @Sql({"/data.sql"})
-    void findMostSpendingDetailsByDate_GivenDate_ReturnSpendingDetails() {
+    void findMostSpendingDetailsByDate_GivenDate_Return() {
+        Date startDate = new Date(2001, 06, 1);
+        Date endDate = new Date(2001, 06, 30);
+        List<UserSpendingDetails> spendings = spendingRepository.findMostSpendingDetailsByDate(startDate , endDate);
+        assertNotNull(spendings);
+        assertEquals(1 , spendings.size());
     }
-    }
+}
