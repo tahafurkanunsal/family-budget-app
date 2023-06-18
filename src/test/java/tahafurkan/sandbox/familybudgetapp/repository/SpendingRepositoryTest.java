7e954e4d5a53be8ac7eb82e6c3c2e7bf3c0022d8
Taha Furkan Ünsal <t.furkanunsal@gmail.com>
2023-06-18 13:58:27 +0300

    H2 database was configured for unit tests to run with docker.

--- src/test/java/tahafurkan/sandbox/familybudgetapp/repository/SpendingRepositoryTest.java
+++ src/test/java/tahafurkan/sandbox/familybudgetapp/repository/SpendingRepositoryTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import tahafurkan.sandbox.familybudgetapp.model.Spending;
 
@@ -17,6 +18,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 class SpendingRepositoryTest {
 
     @Autowired
